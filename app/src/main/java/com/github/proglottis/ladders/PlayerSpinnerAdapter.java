8375385a978add5c7b1360fa6c8ee086aa820864
James Fargher <proglottis@gmail.com>
2015-08-23 14:36:38 +1200

    Add image to player spinner

--- app/src/main/java/com/github/proglottis/ladders/PlayerSpinnerAdapter.java
+++ app/src/main/java/com/github/proglottis/ladders/PlayerSpinnerAdapter.java
@@ -4,10 +4,12 @@ import android.content.Context;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
 import android.widget.SpinnerAdapter;
 import android.widget.TextView;
 
 import com.github.proglottis.ladders.data.Player;
+import com.squareup.picasso.Picasso;
 
 /**
  * Created by james on 21/08/15.
@@ -20,6 +22,32 @@ public class PlayerSpinnerAdapter extends BaseAdapter implements SpinnerAdapter 
     public PlayerSpinnerAdapter(Context context, Player[] players) {
         this.context = context;
         this.players = players;
+    }
+
+    static class ViewHolder {
+        ImageView image;
+        TextView name;
+
+        public ViewHolder(View v) {
+            this.image = (ImageView) v.findViewById(R.id.image);
+            this.name = (TextView) v.findViewById(R.id.name);
+        }
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ViewHolder holder;
+        Player player = players[position];
+        if(convertView == null) {
+            convertView = View.inflate(context, R.layout.player_spinner_item, null);
+            holder = new ViewHolder(convertView);
+            convertView.setTag(holder);
+        } else {
+            holder = (ViewHolder) convertView.getTag();
+        }
+        Picasso.with(context).load(player.getUser().getImageURL()).into(holder.image);
+        holder.name.setText(player.getUser().getName());
+        return convertView;
     }
 
     @Override
@@ -35,12 +63,5 @@ public class PlayerSpinnerAdapter extends BaseAdapter implements SpinnerAdapter 
     @Override
     public long getItemId(int position) {
         return players[position].getId().hashCode();
-    }
-
-    @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
-        TextView view = (TextView) View.inflate(context, android.R.layout.simple_list_item_1, null);
-        view.setText(players[position].getUser().getName());
-        return view;
     }
 }
