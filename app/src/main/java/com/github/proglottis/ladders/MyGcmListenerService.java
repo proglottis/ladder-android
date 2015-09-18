dac7eb58b03d572547bfbda583fcbcd7d7850f5f
Steven Tan <steven@powershop.co.nz>
2015-09-18 15:15:08 +1200

    Added message to notification

--- app/src/main/java/com/github/proglottis/ladders/MyGcmListenerService.java
+++ app/src/main/java/com/github/proglottis/ladders/MyGcmListenerService.java
@@ -6,14 +6,19 @@ import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.util.Log;
 
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.github.proglottis.ladders.data.Game;
+import com.github.proglottis.ladders.requests.GameRequest;
 import com.google.android.gms.gcm.GcmListenerService;
 
 /**
  * Created by steventan on 17/09/15.
  */
-public class MyGcmListenerService extends GcmListenerService {
+public class MyGcmListenerService extends GcmListenerService implements Response.ErrorListener, Response.Listener<Game> {
     private static final String COLLAPSE_KEY = "collapse_key";
     private static final String GAME_CONFIRMATION = "game_confirmation";
     private static final String GAME_ID_KEY = "game_id";
@@ -36,26 +41,41 @@ public class MyGcmListenerService extends GcmListenerService {
 
         if (collapse.equals(GAME_CONFIRMATION)) {
             String gameId = data.getString(GAME_ID_KEY);
-            String message = data.getString(MESSAGE_KEY);
-
-            Intent confirmationIntent = new Intent(this, GameActivity.class);
-            confirmationIntent.putExtra(GameActivity.GAME_ID, gameId);
-
-            PendingIntent pendingintent = PendingIntent.getActivity(this, CONFIRMATION_REQUEST_CODE,
-                    confirmationIntent, PendingIntent.FLAG_UPDATE_CURRENT);
-
-            Notification notification = new Notification.Builder(this)
-                    .setDefaults(Notification.DEFAULT_ALL)
-                    .setContentTitle(getString(R.string.app_name))
-                    .setContentText(message)
-                    .setSmallIcon(R.mipmap.ic_launcher)
-                    .setContentIntent(pendingintent)
-                    .getNotification();
-
-            NotificationManager manager = (NotificationManager)
-                    getSystemService(Context.NOTIFICATION_SERVICE);
-            manager.notify(gameId, NOTIFICATION_ID, notification);
-            Log.d(TAG, "Created a notification");
+            requestGame(gameId);
         }
+    }
+
+    private void requestGame(String gameId) {
+        String token = PreferenceManager.getDefaultSharedPreferences(this).getString(getString(R.string.api_token), null);
+        GameRequest req = new GameRequest(gameId, token, this, this);
+        AppController.getInstance().addToRequestQueue(req, TAG);
+    }
+
+    @Override
+    public void onErrorResponse(VolleyError error) {
+
+    }
+
+    @Override
+    public void onResponse(Game response) {
+        Intent confirmationIntent = new Intent(this, GameActivity.class);
+        confirmationIntent.putExtra(GameActivity.GAME_ID, response.getId());
+
+
+        PendingIntent pendingintent = PendingIntent.getActivity(this, CONFIRMATION_REQUEST_CODE,
+                confirmationIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        Notification notification = new Notification.Builder(this)
+                .setDefaults(Notification.DEFAULT_ALL)
+                .setContentTitle(getString(R.string.notification_title, response.getOwner().getName()))
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setContentText(response.getTournament().getName() + "\n" + response.title())
+                .setContentIntent(pendingintent)
+                .getNotification();
+
+        NotificationManager manager = (NotificationManager)
+                getSystemService(Context.NOTIFICATION_SERVICE);
+        manager.notify(response.getId(), NOTIFICATION_ID, notification);
+        Log.d(TAG, "Created a notification");
     }
 }
